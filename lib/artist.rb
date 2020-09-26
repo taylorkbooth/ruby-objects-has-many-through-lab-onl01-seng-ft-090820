5f8d5636403baef60f4d7cab083da4b9ab822ae5
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 15:06:22 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+require "pry"
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = [] 
+
+  def initialize(name)
+    @name = name 
+  
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end 
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    song
+  end
+
+  def songs 
+    Song.all.select {|song| song.artist == self} 
+  end
+
+  def genres 
+    self.songs.map do |song|
+      song.genre
+    end
+    # binding.pry
+  end 
+
+
+end 
